b22fa208ddf9cb9a6a9a0f4a788b44830e7bbd7f
Anthony Sciamanna <asciamanna@gmail.com>
2022-05-20 08:20:00 -0400

    updates naming in the test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,9 +11,9 @@ class GildedRoseTest {
         int startingSellin = 5;
         int startingQuality = 7;
         final Item standardItem = new Item("Elixir of the Mongoose", startingSellin, startingQuality);
-        GildedRose app = new GildedRose(new Item[] {standardItem});
+        GildedRose subject = new GildedRose(new Item[] {standardItem});
 
-        app.updateQuality();
+        subject.updateQuality();
 
         assertThat(standardItem.sellIn).isEqualTo(startingSellin - 1);
         assertThat(standardItem.quality).isEqualTo(startingQuality - 1);
