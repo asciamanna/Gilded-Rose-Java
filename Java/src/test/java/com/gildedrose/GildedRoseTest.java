a56688fd06b59fc05f1425a46a1a7b7d357064c2
Anthony Sciamanna <asciamanna@gmail.com>
2022-09-12 14:13:54 -0400

    adds additional tests for gildedrose logic coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -34,13 +35,33 @@ class GildedRoseTest {
     }
 
     @Test
-    void item_quality_degrades_twice_as_fast_passed_sellin_date() {
-        Item item = new Item("First Standard Item", -1, 4);
+    void item_quality_degrades_twice_as_fast_past_sellin_date() {
+        Item item = new Item("Standard Item", -1, 4);
         GildedRose subject = new GildedRose(new Item[] { item });
 
         subject.updateQuality();
 
         assertThat(item.quality).isEqualTo(2);
+    }
+
+    @Test
+    void item_quality_degrades_by_one_with_1_day_left() {
+        Item item = new Item("Standard Item", 1, 4);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(3);
+    }
+
+    @Test
+    void item_quality_degrades_down_to_0() {
+        Item item = new Item("Standard Item", 4, 1);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isZero();
     }
 
     @Test
@@ -50,7 +71,7 @@ class GildedRoseTest {
 
         subject.updateQuality();
 
-        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.quality).isZero();
     }
 
     @Test
@@ -64,8 +85,38 @@ class GildedRoseTest {
     }
 
     @Test
+    void aged_item_quality_49_increases_up_to_50() {
+        Item item = new Item("Aged Brie", 5, 49);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(50);
+    }
+
+    @Test
     void quality_of_an_item_is_never_greater_than_50() {
         Item item = new Item("Aged Brie", 5, 50);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(50);
+    }
+
+    @Test
+    void aged_item_quality_increases_twice_as_fast_past_sellin_date() {
+        Item item = new Item("Aged Brie", 0, 6);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(8);
+    }
+
+    @Test
+    void aged_item_quality_50_past_sellin_date_does_not_increase() {
+        Item item = new Item("Aged Brie", 0, 50);
         GildedRose subject = new GildedRose(new Item[] { item });
 
         subject.updateQuality();
@@ -104,6 +155,16 @@ class GildedRoseTest {
     }
 
     @Test
+    void backstage_passes_increase_in_quality_by_1_when_there_are_10_days_or_less() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 48);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(49);
+    }
+
+    @Test
     void backstage_passes_increase_in_quality_by_2_when_there_are_10_days_or_less() {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
         GildedRose subject = new GildedRose(new Item[] { item });
@@ -111,6 +172,26 @@ class GildedRoseTest {
         subject.updateQuality();
 
         assertThat(item.quality).isEqualTo(22);
+    }
+
+    @Test
+    void backstage_passes_quality_49_increase_up_to_50_when_there_are_10_days_or_less() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(50);
+    }
+
+    @Test
+    void backstage_passes_increase_in_quality_by_2_when_there_are_6_days_or_less() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 46);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(48);
     }
 
     @Test
@@ -124,12 +205,32 @@ class GildedRoseTest {
     }
 
     @Test
+    void backstage_passes_quality_47_increase_up_to_50_when_there_are_5_days_or_less() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(50);
+    }
+
+    @Test
+    void backstage_passes_quality_49_increase_up_to_50_when_there_are_5_days_or_less() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49);
+        GildedRose subject = new GildedRose(new Item[] { item });
+
+        subject.updateQuality();
+
+        assertThat(item.quality).isEqualTo(50);
+    }
+
+    @Test
     void backstage_passes_quality_is_0_after_concert() {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
         GildedRose subject = new GildedRose(new Item[] { item });
 
         subject.updateQuality();
 
-        assertThat(item.quality).isEqualTo(0);
+        assertThat(item.quality).isZero();
     }
 }
